40906cc8b6f35f1fa05f2d2e7dd01fbe6567fc74
sergey <sso96@mail.ru>
2022-10-02 18:05:24 +0300

    100% Coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,9 +10,21 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {7, 5, 8, 4, 5, 3, 8, 6, 11, 11, 8};
+        long expected = 11;
 
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxArray(){
+        StatisticsService service = new StatisticsService();
+
+        long [] incomesInBillions = {8, git, 8, 4, 5, 3, 8, 6 , 11, 11, 12};
+
+        int expected = 12;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
